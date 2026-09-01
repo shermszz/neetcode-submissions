4edc72be40f1c666ff6371a0cbf5cd7b9c51dba1
Sherman <168536588+shermszz@users.noreply.github.com>
2026-09-01 11:54:47 +0800

    Add: maximum-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-subarray/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # maintain a dp array where each position tells us what the maximum sum is at that point
+        n = len(nums)
+        dp = [-float('inf') for _ in range(n)]
+        dp[0] = nums[0]
+        
+        for i in range(1, n):
+            # the could either take the current value or not
+            # If i take the value, I will add it to my current maximum
+            # Otherwise, if i dont take the value, i will start again at the current value I am at
+            curr_val = nums[i]
+            dp[i] = max(curr_val, dp[i - 1] + curr_val)
+        
+        print("dp state", dp)
+        return max(dp)
